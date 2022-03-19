a5ab40d72de34ce52a5dc36aa463d160d66d7bd5
Sid <siddhartha.chowdhury@seven.one>
2022-03-19 22:54:14 +0100

    fixed padding left scroll

--- proto2.0/src/navigation/scrollLeftLane.ts
+++ proto2.0/src/navigation/scrollLeftLane.ts
@@ -3,10 +3,10 @@ import { INavigationMapElements } from "./types";
 export const scrollLeftLane = ({ vs, lane, item }: INavigationMapElements) => {
   const itemLeftPadding = lane.spaceBetweenItems || 0;
 
-  const item_x1 = item.getBounds().x + itemLeftPadding;
+  const item_x1 = item.getBounds().x - itemLeftPadding;
 
   if (item_x1 < vs.x) {
-    const difference = vs.x - (item_x1 - itemLeftPadding);
+    const difference = vs.x - item_x1;
     if (lane.getBounds().x < difference) lane.x += difference;
   }
 };
